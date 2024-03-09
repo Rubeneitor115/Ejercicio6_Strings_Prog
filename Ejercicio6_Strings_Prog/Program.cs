d1dfe6e23b4384fd294da5bf2e93d0d4070b0b82
DESKTOP-3ASMSNV\Ruben <rubenbernalramos@gmail.com>
2024-03-09 16:19:59 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio6_Strings_Prog/Program.cs
@@ -0,0 +1,44 @@
+﻿/***
+ * Ruben Bernal Ramos
+ * CSI1
+ */
+
+namespace Ejercicio6_Strings_Prog
+{
+    /// <summary>
+    /// Clase principal de la aplicación
+    /// rbr - 090324
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Método principal de la aplicación
+        /// rbr - 090324
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            //Declaro las variables
+            string frase;
+            List<string> lista;
+
+            //Pido la clase por consola
+            Console.WriteLine("Introduzca una Frase: ");
+            frase = Console.ReadLine();
+
+            //Muestro por consola la frase introducida
+            Console.WriteLine("Frase introducida: ");
+            Console.WriteLine(frase);
+
+            //Invierto la frase y la muestro por consola
+            char[] charArray = frase.ToCharArray();
+            Array.Reverse(charArray);
+
+            Console.WriteLine("Frase invertida: ");
+            foreach (char caracter in charArray)
+            {
+                Console.Write(caracter);
+            }
+        }
+    }
+}
